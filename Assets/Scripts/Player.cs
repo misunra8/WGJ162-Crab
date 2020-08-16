3bf758d6b0e8bd28e2f7dc23a2db171d449b7909
Michael Boyle <mbbexley@hotmail.co.uk>
2020-08-16 13:40:28 +0100

    Add moving around

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Make sure to report that we're using a model from
+    // https://poly.google.com/view/2DgM36qZW2u
     public float speed = 2.0f;
     public float rotateSpeed = 0.5f;
 
-    public KeyCode forward, left, right, backwards;
+    public KeyCode forward, left, right, backward;
 
     public Vector3 cameraOffset = new Vector3(0f, 0.5f, 0.5f);
 
@@ -40,6 +42,7 @@ public class Player : MonoBehaviour
             newRotation.x -= 360f;
         }
         newRotation.x = Mathf.Clamp(newRotation.x, -85f, 70f);
+        newRotation.z = 0f;
         transform.rotation = Quaternion.Euler(newRotation);
         cam.transform.localPosition = camLock.transform.position;
         cam.transform.localRotation = transform.localRotation;
@@ -51,10 +54,18 @@ public class Player : MonoBehaviour
         // Get the Y vector as a direction to move forward
         float yAngle = Mathf.Deg2Rad * transform.eulerAngles.y;
         Vector3 direction = new Vector3(Mathf.Sin(yAngle), 0f, Mathf.Cos(yAngle));
+        float yRightAngle = Mathf.Deg2Rad * (transform.eulerAngles.y + 90f);
+        Vector3 rightDirection = new Vector3(Mathf.Sin(yRightAngle), 0f, Mathf.Cos(yRightAngle));
         Debug.Log("Y rotation = " + yAngle +  " | x: " + direction.x + ", z: " + direction.z);
         facing.position = transform.position + direction * 4f;
-        if (Input.GetKey(forward) && !Input.GetKey(backwards)) {
+        if (Input.GetKey(forward) && !Input.GetKey(backward)) {
             transform.position += direction * Time.deltaTime * speed;
+        } else if (Input.GetKey(backward) && !Input.GetKey(forward)) {
+            transform.position -= direction * Time.deltaTime * speed;
+        } else if (Input.GetKey(right) && !Input.GetKey(left)) {
+            transform.position += rightDirection * Time.deltaTime * speed;
+        } else if (Input.GetKey(left) && !Input.GetKey(right)) {
+            transform.position -= rightDirection * Time.deltaTime * speed;
         }
     }
 }
