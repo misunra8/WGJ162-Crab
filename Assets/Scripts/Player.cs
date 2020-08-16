4fc876914bc4035ccafc138072733b1ca7407cf0
Michael Boyle <mbbexley@hotmail.co.uk>
2020-08-16 21:44:45 +0100

    Do double tap dash

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -8,6 +9,12 @@ public class Player : MonoBehaviour
     // https://poly.google.com/view/2DgM36qZW2u
     public float speed = 2.0f;
     public float rotateSpeed = 0.5f;
+
+    public float dashSpeed, dashTimeout, dashRechargeTime;
+    public Image chargeBar;
+
+    private float dashCatchTime, dashRecharge;
+    private bool rightDash, leftDash;
 
     public KeyCode forward, left, right, backward;
 
@@ -33,6 +40,12 @@ public class Player : MonoBehaviour
                 facing = t;
             }
         }
+        rightDash = false;
+        leftDash = false;
+        dashRecharge = 0f;
+        dashCatchTime = 0f;
+        chargeBar.type = Image.Type.Filled;
+        chargeBar.fillMethod = Image.FillMethod.Horizontal;
     }
 
     // Update is called once per frame
@@ -56,16 +69,65 @@ public class Player : MonoBehaviour
         Vector3 direction = new Vector3(Mathf.Sin(yAngle), 0f, Mathf.Cos(yAngle));
         float yRightAngle = Mathf.Deg2Rad * (transform.eulerAngles.y + 90f);
         Vector3 rightDirection = new Vector3(Mathf.Sin(yRightAngle), 0f, Mathf.Cos(yRightAngle));
-        Debug.Log("Y rotation = " + yAngle +  " | x: " + direction.x + ", z: " + direction.z);
+        //Debug.Log("Y rotation = " + yAngle +  " | x: " + direction.x + ", z: " + direction.z);
         facing.position = transform.position + direction * 4f;
+        Vector3 positionalOffset = new Vector3();
         if (Input.GetKey(forward) && !Input.GetKey(backward)) {
-            transform.position += direction * Time.deltaTime * speed;
+            positionalOffset += direction;
         } else if (Input.GetKey(backward) && !Input.GetKey(forward)) {
-            transform.position -= direction * Time.deltaTime * speed;
-        } else if (Input.GetKey(right) && !Input.GetKey(left)) {
-            transform.position += rightDirection * Time.deltaTime * speed;
+            positionalOffset -= direction;
+        }
+        if (Input.GetKey(right) && !Input.GetKey(left)) {
+            positionalOffset += rightDirection;
         } else if (Input.GetKey(left) && !Input.GetKey(right)) {
-            transform.position -= rightDirection * Time.deltaTime * speed;
+            positionalOffset -= rightDirection;
+        }
+
+        if (positionalOffset.magnitude > 0f) {
+            transform.position += positionalOffset.normalized * Time.deltaTime * speed;
+        }
+
+        HandleDash(rightDirection);
+    }
+
+    private void HandleDash(Vector3 rightDirection) {
+        float deltaTime = Time.deltaTime;
+        if (dashRecharge - deltaTime > 0f) {
+            // It's recharging!
+            dashRecharge -= deltaTime;
+            float completed = 1f - (dashRecharge / dashRechargeTime);
+            chargeBar.color = Color.Lerp(Color.red, Color.green, completed);
+            chargeBar.fillAmount = completed;
+            //chargeBar.localScale = new Vector3(Mathf.Lerp(910f, 568f, completed), 1f, 1f);
+            return;
+        } else if (dashRecharge > 0f) {
+            dashRecharge = 0f;
+            rightDash = false;
+            leftDash = false;
+        } else if (dashCatchTime > 0f) {
+            dashCatchTime -= deltaTime;
+        }
+        Rigidbody r = GetComponent<Rigidbody>();
+        if (Input.GetKeyDown(right) && !Input.GetKey(left)) {
+            leftDash = false;
+            if (rightDash && dashCatchTime > 0f) {
+                r.velocity += rightDirection.normalized * dashSpeed;
+                rightDash = false;
+                dashRecharge = dashRechargeTime;
+            } else {
+                rightDash = true;
+                dashCatchTime = dashTimeout;
+            }
+        } else if (Input.GetKeyDown(left) && !Input.GetKey(right)) {
+            rightDash = false;
+            if (leftDash && dashCatchTime > 0f) {
+                r.velocity -= rightDirection.normalized * dashSpeed;
+                leftDash = false;
+                dashRecharge = dashRechargeTime;
+            } else {
+                leftDash = true;
+                dashCatchTime = dashTimeout;
+            }
         }
     }
 }
