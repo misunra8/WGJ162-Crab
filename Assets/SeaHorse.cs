4e1aa48f66f8c407b00ce5cf29bc36d35807be14
Murat Özcan <murat.ozcan@bil.omu.edu.tr>
2020-08-18 01:56:57 +0300

    //seahorse chase added, but not working great

--- Assets/SeaHorse.cs
+++ Assets/SeaHorse.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class SeaHorse : MonoBehaviour
 {
-
+    //public Transform crabForm;
+    public float speed = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,12 @@ public class SeaHorse : MonoBehaviour
     void Update()
     {
         Vector3 crab = GameObject.Find("Geo_Crab").transform.position;
-        float distanceBetween = Mathf.Sqrt(Mathf.Pow((transform.position.x - crab.x), 2) + Mathf.Pow((transform.position.y - crab.y), 2) + Mathf.Pow((transform.position.z - crab.z), 2));
+        //float distanceBetween = Mathf.Sqrt(Mathf.Pow((transform.position.x - crab.x), 2) + Mathf.Pow((transform.position.y - crab.y), 2) + Mathf.Pow((transform.position.z - crab.z), 2));
+        Vector3 dist = crab - transform.position;
+        Vector3 direction = dist.normalized;
+        Vector3 velocity = direction * speed;
+
+        transform.Translate(velocity * Time.deltaTime);
+
     }
 }
