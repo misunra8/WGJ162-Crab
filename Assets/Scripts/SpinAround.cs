d288582321fe462511c4d440e639d59c74aa8386
Michael Boyle <mbbexley@hotmail.co.uk>
2020-08-20 12:41:10 +0100

    Add a loading scene

---
+++ Assets/Scripts/SpinAround.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinAround : MonoBehaviour
+{
+    public GameObject target;
+    public float speed;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(target.transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
