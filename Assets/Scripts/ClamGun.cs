7da9eab2a9fc95c7fb90498cc7d536849a0e4d77
Michael Boyle <mbbexley@hotmail.co.uk>
2020-08-20 11:24:48 +0100

    Add pearl firing

--- Assets/Scripts/ClamGun.cs
+++ Assets/Scripts/ClamGun.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class ClamGun : MonoBehaviour
 {
+    public float reloadTime, fireSpeed;
+
+    private float reloading;
+
+    public Pearl weapon;
+
     public float clamBounceSize, clamBounceTime;
     public AnimationCurve ac;
 
     private bool clamBounceReturn;
     private float movingTime;
     private Transform top, bottom;
+
+    private Player player;
     // Start is called before the first frame update
     void Start()
     {
+        player = FindObjectOfType<Player>();
         foreach (Transform t in transform) {
             if (t.name == "Top") {
                 top = t;
@@ -51,5 +60,15 @@ public class ClamGun : MonoBehaviour
         currentRotation.z = 0f;
         top.localRotation = Quaternion.Euler(-currentRotation);
         bottom.localRotation = Quaternion.Euler(currentRotation);
+
+        reloading -= Time.deltaTime;
+        if (reloading < 0f) {
+            if (Input.GetMouseButton(0)) {
+                reloading = reloadTime;
+                Pearl pearl = Instantiate(weapon);
+                pearl.transform.localPosition = transform.position;
+                pearl.GetComponent<Rigidbody>().velocity = player.transform.forward * fireSpeed;
+            }
+        }
     }
 }
